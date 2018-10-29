618a0e6428a8f2a1323cb2d93686c0817dc52538
ja153903 <jaimeabbariao@gmail.com>
2018-10-29 15:22:29 -0400

    Included markdown editor with conversion capabilities

--- src/MarkdownEditor.js
+++ src/MarkdownEditor.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
+import { Markdown } from 'react-showdown'
 
 /*
 We want to be able to write down markdown editor and also be able to preview the 
 markdown content as regular text side-by-side
 */
 
-class Markdown extends Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <input type='text' name='markdown'></input>
-          <input type='submit' value='Submit'></input>
-        </form>
-      </div>)
-  }
-}
 
 class MarkdownEditor extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdownInput: '#Nothing yet'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      markdownInput: e.target.value 
+    })
+  }
+  
   render() {
     return (
       <div>
         <p>Enter the markdown text here: </p>
-        <Markdown />
+        <div>
+          <form>
+            <textarea
+              type='text' 
+              value={ this.state.markdownInput } 
+              onChange={ e => this.handleChange(e) } />
+          </form>
+        </div>
+        <Markdown markup={ this.state.markdownInput } />
       </div>
     )
   }
